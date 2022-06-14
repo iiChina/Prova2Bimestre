ec5bc75a06cb759d7babfa9e7a9118df44ce10d5
Alex Lopes <antonio.alex@hotmail.com>
2022-06-14 20:21:48 -0300

    Adicionar arquivos de projeto.

---
+++ Prova2Bimestre/Controllers/ProdutoController.cs
@@ -0,0 +1,48 @@
+﻿using Prova2Bimestre.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prova2Bimestre.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ProdutoModel produtoModel = db.Produtos.Find(id);
+
+            if (produtoModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(produtoModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProdutoModel produtoModel = db.Produtos.Find(id);
+            db.Produtos.Remove(produtoModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Prova2Bimestre/Models/ProdutoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prova2Bimestre.Models
+{
+    public class ProdutoModel
+    {
+        public long ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public FornecedorModel Fornecedor { get; set; }
+    }
+}
